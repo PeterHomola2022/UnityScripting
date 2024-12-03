9d48c9def79fb8f1178499c452f9819504b8ab4c
PeterHomola2022 <103371269+PeterHomola2022@users.noreply.github.com>
2024-12-03 19:11:13 +0100

    First Upload

---
+++ HelloWorld.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        name = "Peter";
+        //Output to the console greetings to the world
+        print(name);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
